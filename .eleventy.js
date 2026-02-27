88778542edfcfeb5753a4c5901c84229fda54c7e
PietroArt3 <pietroernesto.artioli@mail.polimi.it>
2026-02-27 18:45:10 +0100

    css

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,12 @@ module.exports = function(eleventyConfig) {
     // Copia CSS e Immagini nella cartella finale senza modificarli
     eleventyConfig.addPassthroughCopy("./src/css");
     eleventyConfig.addPassthroughCopy("./img");
+    eleventyConfig.addPassthroughCopy("src/doc");
 
     return {
         dir: {
             input: "src",    // Le tue bozze sono qui
             output: "public" // Il sito finito andrà qui
         },
-        pathPrefix: "/sito-associazione/"
     };
 };
