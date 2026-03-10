853f024ac83e59869a7fb651e064ab46730a1d28
PietroArt3 <pietroernesto.artioli@mail.polimi.it>
2026-03-10 17:43:51 +0100

    Semplificazione Inseriemnto titolo e testo

--- .eleventy.js
+++ .eleventy.js
@@ -15,32 +15,41 @@ module.exports = function(eleventyConfig) {
 
     module.exports = function(eleventyConfig) {
 
+        // Creiamo i dati globali
         eleventyConfig.addGlobalData("datiPrimoPiano", () => {
-            // IMPORTANTE: Se i tuoi file sono in una sottocartella (es. 'src/img'),
-            // scrivi path.join(__dirname, "src", "img", "in_primo_piano")
-            const cartella = path.join(__dirname, "img", "in_primo_piano");
+            // Specifichiamo il percorso assoluto alla cartella img
+            const cartellaInteressata = path.resolve(__dirname, "img/in_primo_piano");
 
-            // Verifichiamo se i file esistono fisicamente
-            const pathTitolo = path.join(cartella, "titolo.txt");
-            const pathTesto = path.join(cartella, "testo.txt");
+            const fileTitolo = path.join(cartellaInteressata, "titolo.txt");
+            const fileTesto = path.join(cartellaInteressata, "testo.txt");
 
-            let titoloContenuto = null;
-            let testoContenuto = null;
+            let contenutoTitolo = null;
+            let contenutoTesto = null;
 
-            if (fs.existsSync(pathTitolo)) {
-                titoloContenuto = fs.readFileSync(pathTitolo, "utf8").trim();
+            // Controllo se i file esistono e li leggo
+            if (fs.existsSync(fileTitolo)) {
+                contenutoTitolo = fs.readFileSync(fileTitolo, "utf8").trim();
+            }
+            if (fs.existsSync(fileTesto)) {
+                contenutoTesto = fs.readFileSync(fileTesto, "utf8").trim();
             }
 
-            if (fs.existsSync(pathTesto)) {
-                testoContenuto = fs.readFileSync(pathTesto, "utf8").trim();
-            }
-
+            // Restituisco l'oggetto (anche se i valori sono null)
             return {
-                titolo: titoloContenuto,
-                testo: testoContenuto
+                titolo: contenutoTitolo,
+                testo: contenutoTesto
             };
         });
 
+        // Copia la cartella img nel sito finale
         eleventyConfig.addPassthroughCopy("img");
+
+        // Ritorna la configurazione (fondamentale!)
+        return {
+            dir: {
+                input: ".",
+                output: "_site"
+            }
+        };
     };
 };
