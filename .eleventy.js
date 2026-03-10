4998639c6a2f1580b49640f369c9a3469a24212e
PietroArt3 <pietroernesto.artioli@mail.polimi.it>
2026-03-10 17:35:10 +0100

    Semplificazione Inseriemnto titolo e testo

--- .eleventy.js
+++ .eleventy.js
@@ -10,4 +10,31 @@ module.exports = function(eleventyConfig) {
             output: "public" // Il sito finito andrà qui
         },
     };
+    const fs = require("fs");
+    const path = require("path");
+
+    module.exports = function(eleventyConfig) {
+
+        eleventyConfig.addGlobalData("datiPrimoPiano", () => {
+            // Specifica il percorso corretto della cartella
+            const cartella = path.join(__dirname, "img/in_primo_piano/");
+
+            try {
+                return {
+                    // Legge i file se esistono
+                    titolo: fs.existsSync(path.join(cartella, "titolo.txt"))
+                        ? fs.readFileSync(path.join(cartella, "titolo.txt"), "utf8").trim()
+                        : null,
+                    testo: fs.existsSync(path.join(cartella, "testo.txt"))
+                        ? fs.readFileSync(path.join(cartella, "testo.txt"), "utf8").trim()
+                        : null
+                };
+            } catch (e) {
+                // In caso di errore critico (es. cartella mancante), restituisce tutto vuoto
+                return { titolo: null, testo: null };
+            }
+        });
+
+        eleventyConfig.addPassthroughCopy("img");
+    };
 };
