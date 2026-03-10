4d36bba237352dc7a273277e06f71aa515e684cb
PietroArt3 <pietroernesto.artioli@mail.polimi.it>
2026-03-10 17:49:15 +0100

    Semplificazione Inseriemnto titolo e testo

--- .eleventy.js
+++ .eleventy.js
@@ -10,46 +10,8 @@ module.exports = function(eleventyConfig) {
             output: "public" // Il sito finito andrà qui
         },
     };
-    const fs = require("fs");
-    const path = require("path");
-
-    module.exports = function(eleventyConfig) {
-
-        // Creiamo i dati globali
-        eleventyConfig.addGlobalData("datiPrimoPiano", () => {
-            // Specifichiamo il percorso assoluto alla cartella img
-            const cartellaInteressata = path.resolve(__dirname, "img/in_primo_piano");
-
-            const fileTitolo = path.join(cartellaInteressata, "titolo.txt");
-            const fileTesto = path.join(cartellaInteressata, "testo.txt");
-
-            let contenutoTitolo = null;
-            let contenutoTesto = null;
-
-            // Controllo se i file esistono e li leggo
-            if (fs.existsSync(fileTitolo)) {
-                contenutoTitolo = fs.readFileSync(fileTitolo, "utf8").trim();
-            }
-            if (fs.existsSync(fileTesto)) {
-                contenutoTesto = fs.readFileSync(fileTesto, "utf8").trim();
-            }
-
-            // Restituisco l'oggetto (anche se i valori sono null)
-            return {
-                titolo: contenutoTitolo,
-                testo: contenutoTesto
-            };
-        });
-
-        // Copia la cartella img nel sito finale
-        eleventyConfig.addPassthroughCopy("img");
-
-        // Ritorna la configurazione (fondamentale!)
-        return {
-            dir: {
-                input: ".",
-                output: "_site"
-            }
-        };
-    };
+    eleventyConfig.addGlobalData("datiPrimoPiano", {
+        titolo: "TEST TITOLO",
+        testo: "TEST TESTO"
+    });
 };
